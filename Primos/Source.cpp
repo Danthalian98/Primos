79d618275d9c60ea9683da0cf81a3aa8eca46485
Danthalian <a22110090@gmail.com>
2022-10-06 01:16:52 -0500

    No jala

--- Primos/Source.cpp
+++ Primos/Source.cpp
@@ -21,25 +21,22 @@ void Primos::Numeros() {
 	if (i > 0) {
 		for (int j = 1; j <= i; j++) {
 			if (j != 1) {
-				for (int k = 1; k < j/2; k++) {
+				for (int k = 2; k < j / 2; k++) {
 					if (j % k == 0) {
 						t = 0;
 					}
 					else {
 						t = 1;
-					}	
-					cout << "Cosa j= " << j << " Cosa k= " << k <<" j/2= "<<j/2<< endl;
-					cout << j % k << endl;
-					
+					}
 				}
 				if (t == 1) {
-					//cout << j << endl;
+					cout << j << endl;
+					t = 0;
 				}
 			}
 			if (j == 2 || j == 3) {
-				//cout << j << endl;
+				cout << j << endl;
 			}
-			t = 0;
 		}
 	}else {
 		cout << "El numero rango debe ser mayor a 0" << endl;
